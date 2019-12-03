865e1112530044adaa8796ead2672c2eed1cc4fb
RotemPeer1603 <tuti979@gmail.com>
2019-12-03 12:54:23 +0200

    errorMessage

---
+++ src/main/java/com/movies/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.movies;
+
+public class ErrorMessage {
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
